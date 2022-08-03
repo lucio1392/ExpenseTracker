8af3ce76b9e097b20624f81723b66452bb88929a
Vinh The <theptv@tech.est-rouge.com>
2022-08-03 15:18:50 +0700

    Inital project

---
+++ Expense Tracker/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Expense Tracker
+//
+//  Created by Lucio on 03/08/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
